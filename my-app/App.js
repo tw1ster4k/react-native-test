e0ac0a5d57a842d39b8a43077364b7758e03a08b
tw1ster4k <aminurshinov@gmail.com>
2023-06-18 15:32:05 +0500

    Release1.2.0

--- my-app/App.js
+++ my-app/App.js
@@ -13,32 +13,31 @@ export default function App() {
   const [amount, setAmount] = useState(0)
   const [showQuantity, setShowQuantity] = useState(false);
   const quantityAnimation = React.useRef(new Animated.Value(0)).current
-  const endAnimation = React.useRef(new Animated.Value(100)).current
 
   const endStyle = {
-    opacity: quantityAnimation,
     transform: [
       {
-        translateX: endAnimation.interpolate({
-          inputRange: [0, 100],
-          outputRange: [0, 70],
-        }),
+        translateX: quantityAnimation
       },
     ],
-    marginTop:-27, marginLeft:50, height: 28, width: 27,backgroundColor: '#FF7A00'
+    marginTop:-28.75, marginLeft:60, height: 28.75, width: 27,backgroundColor: '#FF7A00',
+    borderBottomRightRadius:10,
+    borderTopRightRadius:10,
+    position:"relative",
+    zIndex:2
   }
 
   const startStyle = {
-    opacity: quantityAnimation,
     transform: [
       {
-        translateX: quantityAnimation.interpolate({
-          inputRange: [0, 100],
-          outputRange: [0, 70],
-        }),
+        translateX: quantityAnimation
       },
     ],
-    marginTop:-27, marginLeft:85, height: 28, width: 27,backgroundColor: '#FF7A00'
+    marginTop:-28.75, marginLeft:60 ,height: 28.75, width: 27,backgroundColor: '#FF7A00',
+    borderBottomRightRadius:10,
+    borderTopRightRadius:10,
+    position:"relative",
+    zIndex:2
   }
 
   const handleIncrement = () => {
@@ -46,47 +45,27 @@ export default function App() {
     setShowQuantity(true);
     Animated.timing(quantityAnimation, {
       toValue: 100,
-      duration: 1000,
+      duration: 250,
       useNativeDriver: true,
     }).start();
   };
 
   const handleDecrement = () => {
-
-    if (amount === 0) {
+    if (amount-1 === 0) {
+      setAmount(0)
       setShowQuantity(false);
-      Animated.timing(endAnimation, {
-        toValue: 200,
-        duration: 1000,
+      Animated.timing(quantityAnimation, {
+        toValue: 50,
+        duration: 250,
         useNativeDriver: true,
       }).start(); }
     else {
-      setAmount(amount - 1);
+      setAmount(amount ? amount - 1 : 0);
     }
   };
 
   const quantityStyle = amount ? startStyle : endStyle
    
-
-
-/*   const animate_state = {
-    start: 0,
-    end:100
-  }
-
-  const value1 = React.useRef(new Animated.Value(animate_state.start)).current
-  const value2 = React.useRef(new Animated.Value(animate_state.end)).current
-
-  const startAnimate = () => {
-    Animated.timing(value1, {toValue:animate_state.end,useNativeDriver: true, duration: 1000 }).start()
-    setAmount(amount+1)
-  }
-
-  const endAnimate = () => {
-    Animated.timing(value2,{toValue:animate_state.start,useNativeDriver: true, duration: 1000 }).start()
-    setAmount(amount-1)
-  } */
-
 
   return (
    <View style={styles.container}>
@@ -97,30 +76,23 @@ export default function App() {
     <Image source={Losos} style={styles.image} />
     <Text style={styles.dish}>Нигири лосось 2 шт.</Text>
     <Text style={styles.description}>Опаленные суши нигири с соусом терияки, луковым кремом и кунжутом.</Text>
-    <View style={{borderRadius: 10, borderColor: "#FF7A00", color:"#fff", backgroundColor: "#555555",width:111, borderWidth:2,display:"flex",justifyContent:"space-around",alignItems:"center",flexDirection:"row", marginTop:12,marginLeft:50}}>
-      {/* style={{height:32,width:55.5,backgroundColor:"#555555",borderColor:"#FF7A00",borderTopLeftRadius:10,borderBottomLeftRadius:10,borderWidth:2,color:"#fff",fontWeight:400,fontSize:14, marginLeft:40,marginTop:12}} */}
-
-        
-        <TouchableOpacity style={{height:"100%",width:55.5, alignItems:'center'}} onPress={handleDecrement}>
-        <Text style={styles.inner}>
+    <View style={{borderBottomLeftRadius:10,borderTopLeftRadius:10, borderColor: "#FF7A00", color:"#fff", backgroundColor: "#555555",width:111, borderWidth:2,display:"flex",justifyContent:"space-around",alignItems:"center",flexDirection:"row", marginTop:12,marginLeft:50,position:"relative", zIndex:3}}>
+        <TouchableOpacity style={{height:"100%",width:55.5, alignItems:'center',}} onPress={handleDecrement}>
+        <Text style={styles.minus}>
           -
         </Text>
       </TouchableOpacity>
-     
       <Text>850 руб</Text>
-      {/* style={{height:32,width:55.5,backgroundColor:"#555555",borderColor:"#FF7A00",borderTopRightRadius:10,borderBottomRightRadius:10,marginTop:-32,marginLeft:93,borderWidth:2,color:"#fff",fontWeight:400,fontSize:14}} */}
       <TouchableOpacity style={{height:"100%",width:55.5, alignItems:'center'}} onPress={handleIncrement}>
-        <Text style={styles.inner}>
+        <Text style={styles.plus}>
           +
         </Text>
       </TouchableOpacity>
     </View>
-    { showQuantity && (
       <Animated.View style={quantityStyle}>
-          <Text style={styles.number}>{amount}</Text>
+          <Text style={styles.number}>{amount ? amount : 1}</Text>
         </Animated.View>
-        )
-        }
+
       
     <StatusBar style="auto" />   
   </View> 
@@ -151,6 +123,7 @@ const styles = StyleSheet.create({
     marginLeft:10,
     marginTop:24,
     textAlign:"center",
+    
   },
   name:{
     marginTop:24,
@@ -165,6 +138,7 @@ const styles = StyleSheet.create({
     position:"relative",
     marginLeft:12.5,
     marginTop:26,
+    zIndex:0  
   },
   dish:{
     fontWeight:600,
@@ -181,12 +155,17 @@ const styles = StyleSheet.create({
     marginLeft:40,
     width:176,
   },
-  inner:{
+  minus:{
     fontWeight:400,
-    fontSize:13,
+    fontSize:20,
     color:"#fff",
-    marginTop:6,
-    marginLeft:3,
+    marginRight:10
+  },
+  plus:{
+    fontWeight:400,
+    fontSize:20,
+    color:"#fff",
+    marginLeft: 10
   },
   quantity:{
     height:32,
